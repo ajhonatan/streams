f3f60b815773ddd3e40194581bdfa195891b338e
Ajhonatan <ajgeocivil@gmail.com>
2025-06-06 10:27:40 -0500

    corrigiendo para netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  //purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   mode: "jit",
   theme: {
